a8d20ceb713ce96edfdfed5ab936c05c895198a7
CHHG9999 <choione999@naver.com>
2022-09-07 22:47:59 +0900

    PT 예약 서비스

---
+++ src/test/java/kr/human/mybatis/MybatisAppTest.java
@@ -0,0 +1,31 @@
+package kr.human.mybatis;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class MybatisAppTest {
+	
+	static SqlSessionFactory sqlSessionFactory;
+	
+	@Before
+	public void beforeClass() {
+		sqlSessionFactory = MybatisApp.getSqlSessionFactory();
+	}
+	
+	@Test
+	public void getSession() {
+		assertNotNull(MybatisApp.getSqlSessionFactory());
+	}
+
+
+	
+}
